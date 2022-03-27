a7184d3deff6463bdb2f91dbcec75840910bd560
sunniboi77 <100485472+sunniboi77@users.noreply.github.com>
2022-03-27 16:04:14 +0200

    1.3 update

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,63 @@
-let pokemonList = [
-    { name: "Bulbasaur", height: 7, types: ['grass','poison','fire'] } ,
-    { name: "Eevee", height: 0.3, types: ['grass','poison','fire'] },
-    { name: "Espeon", height: 0.9, types: ['grass','poison','fire'] }
-];
 
 
 
 document.write("<h1>Hello World</h1>");
 document.write("Have a nice day!");
 
-
-
-for (let i=0; i<pokemonList.length ;i++) {
-    document.writeln('<p>',pokemonList[i].name,' height is    ',pokemonList[i].height,'</p>');
+let pokemonList = [
+    { name: "Bulbasaur", height: 7, types: ['grass', 'poison', 'fire'] },
+    { name: "Eevee", height: 0.3, types: ['grass', 'poison', 'fire'] },
+    { name: "Espeon", height: 0.9, types: ['grass', 'poison', 'fire'] },
+    { name: "Charmander", height: 2, types: ["fire"] },
+    { name: "Squirtle", height: 1, types: ["water"] },
+    { name: "Caterpie", height: 1, types: ["bug"] },
+    { name: "Weedle", height: 1, ypes: ["bug", "poison"] }
+];
+ 
+for (let i=0; i < pokemonList.length ;i++) {
+    if ( (pokemonList[i].height > 1.5 ) ) {
+        document.writeln("<p>", pokemonList[i].name, ' height is BIGGER than ',pokemonList[i].height , '<\p>');    
+    }
+    else {
+        document.writeln("<div>", pokemonList[i].name, ' height is  ',pokemonList[i].height , '<\div>');    
+     
+    }
 }
+ 
 
 
-function newContent() {
-    document.open();
-    document.write("<h1>Out with the old, in with the new!</h1>");
-    document.close();
-}
+/* NEXT EXERCISE CODE 
+// Loop to list all the pokemons on the page
 
-function myFunction() {
-    document.open();
-    document.write("<h1>Hello World</h1>");
-    document.close();
-}
+	// Defining getAll function to return pokemonList
+	function getAll () {
+		return pokemonList;
+	}
 
-/* SOME TEST  
+	// Defining add function to add to pokemonList
+	function add(pokemon) {
+		pokemonList.push(pokemon);
+	}
 
- var myarray = new Array();
-for (i = 0; i < 10; i++)
-{
-    myarray[i] = i;
-}
-document.write(myarray);
-
-
-
-
-
-var person={
-    first_name:"johnny",
-     last_name: "johnson",
-   phone:"703-3424-1111"
-}
-
-
-for (var property in person) {
-    console.log(property,":",person[property]);
-}
-
-
+	// Returning getAll and add functions
+	return {
+		getAll: getAll,
+		add: add
+	}
 
 */
+
  
+
+/*
+
+'<p>','This is a BIG one',pokemonList[i].name,' height is    ',pokemonList[i]height,'</p>'
+for (let i=0; i < pokemonList.length ;i++) {
+    if (pokemonList[i].height === ) {
+        document.writeln('<p>','This is a BIG one',pokemonList[i].name,' height is    ',pokemonList[i]height,'</p>');
+    } else {
+        document.writeln('<p>', pokemonList[i].name,' height is    ',pokemonList[i].height,'</p>');
+    }
+}
+*/
+
