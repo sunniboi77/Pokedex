d7d03f40f0e3c58ebac62683514492ffce1ce3ee
sunniboi77 <100485472+sunniboi77@users.noreply.github.com>
2022-04-06 21:31:25 +0200

    1.6 Done

--- js/scriptsv2.js
+++ js/scriptsv2.js
@@ -1,53 +1,72 @@
-
-
-// document.write("<h1>Hello World</h1>");
-// document.write("Have a nice day!");
+document.write("<h1>this text is from script</h1>");
+document.write("Have a nice day!");
 
 let PokemonRepository = (function () {
     let pokemonList = [     
-        { name: "Bulbasaur", height: 7, types: ['grass', 'poison', 'fire']},
-        { name: "Eevee", height: 0.3, types: ['grass', 'poison', 'fire'] },
-        { name: "Espeon", height: 0.9, types: ['grass', 'poison', 'fire'] },
-        { name: "Charmander", height: 2, types: ["fire"] },
-        { name: "Squirtle", height: 1, types: ["water"] },
-        { name: "Caterpie", height: 1, types: ["bug"] },
-        { name: "Weedle", height: 1, types: ["bug", "poison"] },
-    ]
+        { name: "Bulbasaur", height: 0.7, types: ["grass", "poison", "fire"], },
+        { name: "Eevee", height: 0.3, types: ["grass", "poison", "fire"], },
+        { name: "Espeon", height: 0.9, types: ["grass", "poison", "fire"], },
+        { name: "Charmander", height: 2, types: ["fire"], },
+        { name: "Squirtle", height: 1, types: ["water"], },
+        { name: "Caterpie", height: 1, types: ["bug"], },
+        { name: "Weedle", height: 1, types: ["bug", "poison"], },
+        { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"], },
+        { name: "Charizard",height: 1.7, types: ["fire", "flying"],},
+        { name: "Squirtle", height: 1, types: ["water"],},
+    ];
 
-    function add(pokemon){
-        pokemonList.push(pokemon);
+    function add(pokemon) {
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "types" in pokemon
+        ) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log("pokemon is not correct");
+        }
     }
-
-    function getAll(){
+    function getAll() {
         return pokemonList;
     }
-  
+
+    function showDetails(pokemon){
+        console.log("showdetails function", pokemon.target.innerHTML);
+    }
+
+    function addListItem(pokemon){
+        console.log(pokemon,"addlistitem funkcio pokemon parametere");
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon  = document.createElement("li"); 
+        let button    = document.createElement("button");
+        
+        button.innerText = pokemon.name;
+        button.classList.add("pokeButton-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+        
+        button.addEventListener('click',showDetails)
+        
+    }
     return{
-        add, getAll
-    }
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem
+    };
+})();
 
-}) ();
+PokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
 
+console.log(PokemonRepository.getAll());
 
-let obj1 = PokemonRepository.getAll();
-
-PokemonRepository.add({ name: "Weedle2", height: 1, types: ["bug", "poison"] });
-PokemonRepository.add({ name: "Weedle3", height: 1, types: ["bug", "poison"] });
-
-console.log(typeof(obj1)); // object
-
-console.log(obj1); 
-
-// Loop to list all the pokemons on the page
-PokemonRepository.getAll().forEach (function(pokemon) {
-    // Conditional format if Pokemon size is over 1.5
-	if (pokemon.height > 1.5) {
-		document.write("<p class=biggie>" + "Name: " + pokemon.name + " (Height: " + pokemon.height + ". Types: " + pokemon.types + ".)"+ "</p>");
-	} 
-    else {
-    document.write("<p>"+ "Name: " + pokemon.name + " (Height: " + pokemon.height + ". Types: " + pokemon.types + ".)"+"</p>");
-    }
-	document.write("<br>");
+PokemonRepository.getAll().forEach(function (pokemon) {
+    console.log('this is pokemon tessek', pokemon);
+    PokemonRepository.addListItem(pokemon);
 });
-	
- 
+
+
+let element = document.querySelector('select');
+console.log(element.value); // returns '1' as that’s currently selected
+element.value = '2';
+
